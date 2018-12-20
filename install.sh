f55fd6d01becab9a424cd320f085ebf589a06b8b
Takuya Shizukuishi <b1015208@fun.ac.jp>
2018-12-20 23:17:32 +0900

    [add] terminator config

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/.dircolors ~/.dircolors
 
+if [ -e ~/.config/terminator ]; then
+  ln -sf $(pwd)/terminator/config ~/.config/terminator/config
+fi
+
--- uninstall.sh
+++ uninstall.sh
@@ -6,3 +6,7 @@ rm -rf ~/.vimrc
 rm -rf ~/.gitconfig
 rm -rf ~/.dircolors
 
+if [ -e ~/.config/terminator ]; then
+  rm -rf ~/.config/terminator/config
+fi
+
