1cb16c864a4d7fa7346c6110711359bc8e4359ae
Takuya Shizukuishi <sititou70@gmail.com>
2020-03-12 23:03:08 +0900

    feat: tmux config

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -sf $(pwd)/.bashrc ~/.bashrc
 ln -sf $(pwd)/.zshrc ~/.zshrc
 ln -sf $(pwd)/.vimrc ~/.vimrc
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/.dircolors ~/.dircolors
 
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 rm -rf ~/.bashrc
 rm -rf ~/.zshrc
 rm -rf ~/.vimrc
+rm -rf ~/.tmux.conf
 rm -rf ~/.gitconfig
 rm -rf ~/.dircolors
 
