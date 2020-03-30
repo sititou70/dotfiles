5e7bc9adaef7af31a04e4eab0f4c8453b8a7e5f4
Takuya Shizukuishi <sititou70@gmail.com>
2020-03-30 19:01:11 +0900

    fix: uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ IFS="
 "
 
 for i in $FILES; do
-  dest_file=$(echo "$i" | sed -e "s/->/ /" | cut -d " " -f 2)
+  dest_file=$(echo "$i" | sed -e "s/->/@/" | cut -d "@" -f 2)
   
   echo uninstall: $dest_file
   rm -rf $dest_file
