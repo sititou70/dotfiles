8687fe7e12d87752db1a6b606296f870e717b3cb
Takuya Shizukuishi <sititou70@gmail.com>
2024-07-11 23:33:00 +0900

    feat: support fuzzy path

--- install.sh
+++ install.sh
@@ -5,18 +5,27 @@ cd $(dirname $0)
 . ./consts.sh
 
 # main
-echo "$FILES" | while read item; do
+print_args() {
+  for arg in "$@"; do
+    echo "$arg"
+  done
+}
+
+echo "$FILES" | while read -r item; do
   [ "$item" = "" ] && continue
 
-  source_file=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 1)
-  dest_file=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 2)
-  dest_dir=$(dirname "$dest_file")
+  source_path=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 1)
+  dest_pattern=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 2)
+  dest_file=$(basename "$dest_pattern")
+  dest_dir_pattern=$(dirname "$dest_pattern")
 
-  if [ -e "$dest_dir" ]; then
-    rm -rf "$dest_file"
-    ln -sf "$(pwd)/$source_file" "$dest_file"
-    printf "${GREEN}installed${RESET}: $source_file -> $dest_file\n"
-  else
-    printf "${RED}failed${RESET}:    $source_file: $dest_dir not found\n"
-  fi
+  eval "print_args $dest_dir_pattern" | while read -r dest_dir; do
+    if [ -e "$dest_dir" ]; then
+      rm -rf "$dest_dir/$dest_file"
+      ln -sf "$(pwd)/$source_path" "$dest_dir/$dest_file"
+      printf "${GREEN}installed${RESET}: $source_path -> $dest_dir/$dest_file\n"
+    else
+      printf "${RED}failed${RESET}:    $item\n"
+    fi
+  done
 done
--- uninstall.sh
+++ uninstall.sh
@@ -5,11 +5,23 @@ cd $(dirname $0)
 . ./consts.sh
 
 # main
-echo "$FILES" | while read item; do
+print_args() {
+  for arg in "$@"; do
+    echo "$arg"
+  done
+}
+
+echo "$FILES" | while read -r item; do
   [ "$item" = "" ] && continue
 
-  dest_file=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 2)
+  dest_pattern=$(echo "$item" | sed -e "s/ -> /:/" | cut -d ":" -f 2)
+  dest_file=$(basename "$dest_pattern")
+  dest_dir_pattern=$(dirname "$dest_pattern")
 
-  rm -rf "$dest_file"
-  echo "uninstalled: $dest_file"
+  eval "print_args $dest_dir_pattern" | while read -r dest_dir; do
+    if [ -e "$dest_dir" ]; then
+      rm -rf "$dest_dir/$dest_file"
+      echo "uninstalled: $dest_dir/$dest_file"
+    fi
+  done
 done
